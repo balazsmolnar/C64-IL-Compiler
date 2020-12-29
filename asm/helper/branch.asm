eb5f41a527897a13d40ffe0f401024bab458fd4d
Balazs Molnar <balazsm@gmail.com>
2020-12-29 20:10:26 +0100

    use short branches if possible

--- asm/helper/branch.asm
+++ asm/helper/branch.asm
@@ -1,15 +1,42 @@
-!macro branch_true label {
-    +stack_pull_int $30
-    beq+
-    jmp label
-+       
 
+!macro bne .label {
+    !set .d = 0
+    !ifndef .label  {
+        !set d = 255
+    } else {
+        !set d = * - .label
+    }
+    !if (.d < 127) {
+        bne .label
+    } else {
+        beq+
+        jmp .label
++       
+    }
 }
 
-!macro branch_false label {
-    +stack_pull_int $30
-    bne+
-    jmp label
+!macro beq .label {
+    !set .d = 0
+    !ifndef .label  {
+        !set d = 255
+    } else {
+        !set d = * - .label
+    }
+    !if (.d < 127) {
+        beq .label
+    } else {
+        bne+
+        jmp .label
 +       
+    }
+}
 
+!macro branch_true .label {
+    +stack_pull_int $30
+    +bne .label
+}
+
+!macro branch_false .label {
+    +stack_pull_int $30
+    +beq .label
 }
